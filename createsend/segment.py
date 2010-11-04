c6764167640481c46b999553792b4b6960e5c267
James Dennes <jdennes@gmail.com>
2010-11-05 08:39:21 +1100

    Adding current segment functionality and tests.

---
+++ createsend/segment.py
@@ -0,0 +1,25 @@
+import json
+from createsend import CreateSendBase
+from utils import json_to_py
+
+class Segment(CreateSendBase):
+
+***REMOVED***def __init__(self, segment_id=None):
+***REMOVED******REMOVED***self.segment_id = segment_id
+***REMOVED******REMOVED***super(Segment, self).__init__()
+
+***REMOVED***def subscribers(self, date, page=1, page_size=1000, order_field="email", order_direction="asc"):
+***REMOVED******REMOVED***params = {
+***REMOVED******REMOVED******REMOVED***"date": date,
+***REMOVED******REMOVED******REMOVED***"page": page,
+***REMOVED******REMOVED******REMOVED***"pagesize": page_size,
+***REMOVED******REMOVED******REMOVED***"orderfield": order_field,
+***REMOVED******REMOVED******REMOVED***"orderdirection": order_direction }
+***REMOVED******REMOVED***response = self._get(self.uri_for("active"), params=params)
+***REMOVED******REMOVED***return json_to_py(response)
+
+***REMOVED***def delete(self):
+***REMOVED******REMOVED***response = self._delete("/segments/%s.json" % self.segment_id)
+
+***REMOVED***def uri_for(self, action):
+***REMOVED******REMOVED***return "/segments/%s/%s.json" % (self.segment_id, action)
---
+++ test/test_segment.py
@@ -0,0 +1,31 @@
+import unittest
+
+from createsend import Segment
+
+class SegmentTestCase(unittest.TestCase):
+
+***REMOVED***def setUp(self):
+***REMOVED******REMOVED***self.list_id = "98y2e98y289dh89h938389"
+***REMOVED******REMOVED***self.segment = Segment(self.list_id)
+
+***REMOVED***def test_subscribers(self):
+***REMOVED******REMOVED***min_date = "2010-01-01"
+***REMOVED******REMOVED***self.segment.stub_request("segment_subscribers.json")
+***REMOVED******REMOVED***res = self.segment.subscribers(min_date)
+***REMOVED******REMOVED***self.assertEquals(res.ResultsOrderedBy, "email")
+***REMOVED******REMOVED***self.assertEquals(res.OrderDirection, "asc")
+***REMOVED******REMOVED***self.assertEquals(res.PageNumber, 1)
+***REMOVED******REMOVED***self.assertEquals(res.PageSize, 1000)
+***REMOVED******REMOVED***self.assertEquals(res.RecordsOnThisPage, 2)
+***REMOVED******REMOVED***self.assertEquals(res.TotalNumberOfRecords, 2)
+***REMOVED******REMOVED***self.assertEquals(res.NumberOfPages, 1)
+***REMOVED******REMOVED***self.assertEquals(len(res.Results), 2)
+***REMOVED******REMOVED***self.assertEquals(res.Results[0].EmailAddress, "personone@example.com")
+***REMOVED******REMOVED***self.assertEquals(res.Results[0].Name, "Person One")
+***REMOVED******REMOVED***self.assertEquals(res.Results[0].Date, "2010-10-27 13:13:00")
+***REMOVED******REMOVED***self.assertEquals(res.Results[0].State, "Active")
+***REMOVED******REMOVED***self.assertEquals(res.Results[0].CustomFields, [])
+
+***REMOVED***def test_delete(self):
+***REMOVED******REMOVED***self.segment.stub_request(None)
+***REMOVED******REMOVED***self.segment.delete()
