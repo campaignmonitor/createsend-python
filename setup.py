b7e4346a18326d16ccdc68de9c86dc91d8c28ecf
James Dennes <jdennes@gmail.com>
2010-11-10 17:13:25 +1100

    Improving description.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from createsend import __version__
 
 setup(name = "createsend",
 ***REMOVED******REMOVED******REMOVED***version = __version__,
-***REMOVED******REMOVED******REMOVED***description = "A wrapper for the CreateSend API v3",
+***REMOVED******REMOVED******REMOVED***description = "A library which implements the complete functionality of v3 of the createsend API.",
 ***REMOVED******REMOVED******REMOVED***author = "James Dennes",
 ***REMOVED******REMOVED******REMOVED***author_email = 'jdennes@gmail.com',
 ***REMOVED******REMOVED******REMOVED***packages = ['createsend'])
