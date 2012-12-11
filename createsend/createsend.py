3e63713d9acf85286f47dfcb0f6fd9a16aca91c3
James Dennes <jdennes@gmail.com>
2012-12-11 14:19:29 +1100

    Version 2.5.0.

--- createsend/createsend.py
+++ createsend/createsend.py
@@ -7,7 +7,7 @@ from StringIO import StringIO
 from urlparse import urlparse
 from utils import json_to_py, get_faker
 
-__version_info__ = ('2', '4', '1')
+__version_info__ = ('2', '5', '0')
 __version__ = '.'.join(__version_info__)
 
 class CreateSendError(Exception):
