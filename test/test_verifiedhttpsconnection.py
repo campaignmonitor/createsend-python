a0d43e021ebb4d854b5f56717481766eaf54719f
James Dennes <jdennes@gmail.com>
2013-07-12 19:13:28 +0200

    Another test for match_hostname.

--- test/test_verifiedhttpsconnection.py
+++ test/test_verifiedhttpsconnection.py
@@ -1,11 +1,15 @@
 import unittest
 
 ***REMOVED***
+from createsend.utils import match_hostname
 
 class VerifiedHTTPSConnectionTestCase(unittest.TestCase):
 
 ***REMOVED***def setUp(self):
 ***REMOVED******REMOVED***self.cs = CreateSend({'api_key': 'not an api key'})
+
+***REMOVED***def test_verified_connection_no_cert(self):
+***REMOVED******REMOVED***self.assertRaises(ValueError, match_hostname, None, 'api.createsend.com')
 
 ***REMOVED***def test_verified_connection(self):
 ***REMOVED******REMOVED***# An actual (non-stubbed) unauthenticated request to test verification.
