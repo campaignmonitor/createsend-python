fcec0363414b94ea87809bb723f10ff2345ea3ec
James Dennes <jdennes@gmail.com>
2013-07-12 18:16:07 +0200

    Added a functional test for using VerifiedHTTPSConnectionTestCase.

---
+++ test/test_verifiedhttpsconnection.py
@@ -0,0 +1,12 @@
+import unittest
+
+***REMOVED***
+
+class VerifiedHTTPSConnectionTestCase(unittest.TestCase):
+
+***REMOVED***def setUp(self):
+***REMOVED******REMOVED***self.cs = CreateSend({'api_key': 'not an api key'})
+
+***REMOVED***def test_verified_connection(self):
+***REMOVED******REMOVED***# An actual (non-stubbed) unauthenticated request to test verification.
+***REMOVED******REMOVED***self.assertRaises(Unauthorized, self.cs.clients)
