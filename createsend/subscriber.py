423990030df02865c28b8a2ab979c99af6d6b452
James Dennes <jdennes@gmail.com>
2014-01-25 20:25:40 +0100

    Remove the need for unnecessary args when calling Subscriber.get.
    This allows the following code:
    
    s = Subscriber('listid', 'me@test.com').get()
    
    Rather than this, which was previously required:
    
    s = Subscriber('listid', 'me@test.com').get('listid', 'me@test.com')

--- createsend/subscriber.py
+++ createsend/subscriber.py
@@ -13,10 +13,10 @@ class Subscriber(CreateSendBase):
     self.email_address = email_address
     super(Subscriber, self).__init__(auth)
 
-  def get(self, list_id, email_address):
+  def get(self, list_id=None, email_address=None):
     """Gets a subscriber by list ID and email address."""
-    params = { "email": email_address }
-    response = self._get("/subscribers/%s.json" % list_id, params=params)
+    params = { "email": email_address or self.email_address }
+    response = self._get("/subscribers/%s.json" % (list_id or self.list_id), params=params)
     return json_to_py(response)
 
   def add(self, list_id, email_address, name, custom_fields, resubscribe, restart_subscription_based_autoresponders=False):
@@ -29,9 +29,9 @@ class Subscriber(CreateSendBase):
       "RestartSubscriptionBasedAutoresponders": restart_subscription_based_autoresponders }
     response = self._post("/subscribers/%s.json" % list_id, json.dumps(body))
     return json_to_py(response)
-  
+
   def update(self, new_email_address, name, custom_fields, resubscribe, restart_subscription_based_autoresponders=False):
-    """Updates any aspect of a subscriber, including email address, name, and 
+    """Updates any aspect of a subscriber, including email address, name, and
     custom field data if supplied."""
     params = { "email": self.email_address }
     body = {
@@ -40,7 +40,7 @@ class Subscriber(CreateSendBase):
       "CustomFields": custom_fields,
       "Resubscribe": resubscribe,
       "RestartSubscriptionBasedAutoresponders": restart_subscription_based_autoresponders }
-    response = self._put("/subscribers/%s.json" % self.list_id, 
+    response = self._put("/subscribers/%s.json" % self.list_id,
       body=json.dumps(body), params=params)
     # Update self.email_address, so this object can continue to be used reliably
     self.email_address = new_email_address
@@ -57,7 +57,7 @@ class Subscriber(CreateSendBase):
     except BadRequest, br:
       # Subscriber import will throw BadRequest if some subscribers are not imported
       # successfully. If this occurs, we want to return the ResultData property of
-      # the BadRequest exception (which is of the same "form" as the response we'd 
+      # the BadRequest exception (which is of the same "form" as the response we'd
       # receive upon a completely successful import)
       if hasattr(br.data, 'ResultData'):
         return br.data.ResultData
