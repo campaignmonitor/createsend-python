7d1ce810448f50388b73ba1f8c0e4e51f7b0abda
djstein <djstein@ncsu.edu>
2017-06-12 17:51:36 -0400

    Updated init imports, add version file

--- createsend/__init__.py
+++ createsend/__init__.py
@@ -1,1 +1,20 @@
-﻿
+﻿# -*- coding: utf-8 -*-
+__title__ = 'createsend-python'
+__author__ = 'Dylan Stein'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017'
+
+
+from createsend.createsend import *
+from createsend.client import Client
+from createsend.template import Template
+from createsend.list import List
+from createsend.segment import Segment
+from createsend.subscriber import Subscriber
+from createsend.campaign import Campaign
+from createsend.person import Person
+from createsend.administrator import Administrator
+from createsend.transactional import Transactional
+from createsend import utils
+
+from createsend.version import __version__
