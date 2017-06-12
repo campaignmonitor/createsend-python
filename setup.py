f7401c626e79aae01aa838fa4c30858ae560b517
djstein <djstein@ncsu.edu>
2017-06-12 17:42:21 -0400

    add instlal requires to setup.py for six

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 
 from createsend.createsend import __version__
 
-setup(name="createsend",
-***REMOVED******REMOVED******REMOVED***version=__version__,
-***REMOVED******REMOVED******REMOVED***description="A library which implements the complete functionality of the Campaign Monitor API.",
-***REMOVED******REMOVED******REMOVED***author="James Dennes",
-***REMOVED******REMOVED******REMOVED***author_email='jdennes@gmail.com',
-***REMOVED******REMOVED******REMOVED***url="http://campaignmonitor.github.io/createsend-python/",
-***REMOVED******REMOVED******REMOVED***license="MIT",
-***REMOVED******REMOVED******REMOVED***keywords="createsend campaign monitor email",
-***REMOVED******REMOVED******REMOVED***packages=['createsend'],
-***REMOVED******REMOVED******REMOVED***package_data={'createsend': ['cacert.pem']})
+setup(
+***REMOVED******REMOVED***name="createsend",
+***REMOVED******REMOVED***version=__version__,
+***REMOVED******REMOVED***description="A library which implements the complete functionality of the Campaign Monitor API.",
+***REMOVED******REMOVED***author="Dylan Stein",
+***REMOVED******REMOVED***author_email='djstein@ncsu.edu',
+***REMOVED******REMOVED***url="http://campaignmonitor.github.io/createsend-python/",
+***REMOVED******REMOVED***license="MIT",
+***REMOVED******REMOVED***keywords="createsend campaign monitor email",
+***REMOVED******REMOVED***packages=['createsend'],
+***REMOVED******REMOVED***package_data={'createsend': ['cacert.pem']},
+***REMOVED******REMOVED***install_requires=['six'],
+)
