df1db35506df47ef68b861fbc8859ad336666d6a
djstein <djstein@ncsu.edu>
2017-06-12 17:42:21 -0400

    add instlal requires to setup.py for six

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 
 from createsend.createsend import __version__
 
-setup(name="createsend",
-      version=__version__,
-      description="A library which implements the complete functionality of the Campaign Monitor API.",
-      author="James Dennes",
-      author_email='jdennes@gmail.com',
-      url="http://campaignmonitor.github.io/createsend-python/",
-      license="MIT",
-      keywords="createsend campaign monitor email",
-      packages=['createsend'],
-      package_data={'createsend': ['cacert.pem']})
+setup(
+    name="createsend",
+    version=__version__,
+    description="A library which implements the complete functionality of the Campaign Monitor API.",
+    author="Dylan Stein",
+    author_email='djstein@ncsu.edu',
+    url="http://campaignmonitor.github.io/createsend-python/",
+    license="MIT",
+    keywords="createsend campaign monitor email",
+    packages=['createsend'],
+    package_data={'createsend': ['cacert.pem']},
+    install_requires=['six'],
+)
