d4eb9dfe1136e3ed1a1e46d15f81dfe1ba92f278
djstein <djstein@ncsu.edu>
2017-07-13 17:44:59 -0400

    explicitly define all imports

--- createsend/__init__.py
+++ createsend/__init__.py
@@ -3,3 +3,32 @@ __title__ = 'createsend-python'
 __author__ = 'Dylan Stein'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017'
+
+
+from createsend.utils import (
+    CertificateError,
+    VerifiedHTTPSConnection,
+    json_to_py,
+    dict_to_object,
+    get_faker)
+from createsend.administrator import Administrator
+from createsend.campaign import Campaign
+from createsend.client import Client
+from createsend.createsend import (
+    CreateSendError,
+    ClientError,
+    ServerError,
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    Unavailable,
+    ExpiredOAuthToken,
+    CreateSendBase,
+    CreateSend)
+from createsend.list import List
+from createsend.person import Person
+from createsend.segment import Segment
+from createsend.subscriber import Subscriber
+from createsend.template import Template
+from createsend.transactional import Transactional
+from createsend.version import __version__
