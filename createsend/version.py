7f9a375220a7770242231b0c8129fe6d2b2c4452
djstein <djstein@ncsu.edu>
2017-07-13 17:48:56 -0400

    version 4.2.3

--- createsend/version.py
+++ createsend/version.py
@@ -6,5 +6,5 @@ __author__ = 'Dylan Stein'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017'
 
-version_info = (4, 2, 1)
+version_info = (4, 2, 3)
 __version__ = ".".join(map(str, version_info))
