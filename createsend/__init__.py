a9599c09b13b54376e87ab1e6f19947d8aa4fe68
Dylan <djstein@ncsu.edu>
2017-07-13 23:38:52 -0400

    All imports from __init__.py are now explicit (#46)
    * remove imports to all other files
    
    * explicitly define all imports

--- createsend/__init__.py
+++ createsend/__init__.py
@@ -5,16 +5,30 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2017'
 
 
-from createsend.createsend import *
+from createsend.utils import (
+***REMOVED******REMOVED***CertificateError,
+***REMOVED******REMOVED***VerifiedHTTPSConnection,
+***REMOVED******REMOVED***json_to_py,
+***REMOVED******REMOVED***dict_to_object,
+***REMOVED******REMOVED***get_faker)
+from createsend.administrator import Administrator
+from createsend.campaign import Campaign
 from createsend.client import Client
-from createsend.template import Template
+from createsend.createsend import (
+***REMOVED******REMOVED***CreateSendError,
+***REMOVED******REMOVED***ClientError,
+***REMOVED******REMOVED***ServerError,
+***REMOVED******REMOVED***BadRequest,
+***REMOVED******REMOVED***Unauthorized,
+***REMOVED******REMOVED***NotFound,
+***REMOVED******REMOVED***Unavailable,
+***REMOVED******REMOVED***ExpiredOAuthToken,
+***REMOVED******REMOVED***CreateSendBase,
+***REMOVED******REMOVED***CreateSend)
 from createsend.list import List
+from createsend.person import Person
 from createsend.segment import Segment
 from createsend.subscriber import Subscriber
-from createsend.campaign import Campaign
-from createsend.person import Person
-from createsend.administrator import Administrator
+from createsend.template import Template
 from createsend.transactional import Transactional
-from createsend import utils
-
 from createsend.version import __version__
