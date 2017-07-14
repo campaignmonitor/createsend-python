1ee0b24f3f05ab7106427d7c3f3cb9838dbcf8d9
Dylan <djstein@ncsu.edu>
2017-07-13 23:38:52 -0400

    All imports from __init__.py are now explicit (#46)
    * remove imports to all other files
    
    * explicitly define all imports

--- createsend/__init__.py
+++ createsend/__init__.py
@@ -5,16 +5,30 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2017'
 
 
-from createsend.createsend import *
+from createsend.utils import (
+    CertificateError,
+    VerifiedHTTPSConnection,
+    json_to_py,
+    dict_to_object,
+    get_faker)
+from createsend.administrator import Administrator
+from createsend.campaign import Campaign
 from createsend.client import Client
-from createsend.template import Template
+from createsend.createsend import (
+    CreateSendError,
+    ClientError,
+    ServerError,
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    Unavailable,
+    ExpiredOAuthToken,
+    CreateSendBase,
+    CreateSend)
 from createsend.list import List
+from createsend.person import Person
 from createsend.segment import Segment
 from createsend.subscriber import Subscriber
-from createsend.campaign import Campaign
-from createsend.person import Person
-from createsend.administrator import Administrator
+from createsend.template import Template
 from createsend.transactional import Transactional
-from createsend import utils
-
 from createsend.version import __version__
