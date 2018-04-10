ace822b8c27074d24c9914cdfb24f4bd8b386628
Katie Bell <katie@katharos.id.au>
2018-04-10 13:08:57 +1000

    Correct the version number in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="createsend",
-    version='4.3.0',
+    version='5.0.0',
     description="A library which implements the complete functionality of the Campaign Monitor API.",
     author='Campaign Monitor',
     author_email='support@campaignmonitor.com',
