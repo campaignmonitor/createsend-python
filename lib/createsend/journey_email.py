f858c9d8ad9d1d3be92a10f54aa03ea51852b4f3
Zachary Hartmann <hartmann.zachary@gmail.com>
2019-08-28 16:44:02 +1000

    Increased test coverage and handled parameters more efficiently

--- lib/createsend/journey_email.py
+++ lib/createsend/journey_email.py
@@ -13,58 +13,73 @@ class JourneyEmail(CreateSendBase):
         self.journey_email_id = journey_email_id
         super(JourneyEmail, self).__init__(auth)
 
-    def bounces(self, date="", page=1, page_size=1000, order_field="date", order_direction="asc"):
+    def bounces(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the bounces for this journey email."""
-        params = {
-            "date": date,
-            "page": page,
-            "pagesize": page_size,
-            "orderfield": order_field,
-            "orderdirection": order_direction}
+        params = {}
+        if date is not None:
+            params["date"] = date
+        if page is not None:
+            params["page"] = page
+        if page_size is not None:
+            params["pagesize"] = page_size
+        if order_direction is not None:
+            params["orderdirection"] = order_direction
         response = self._get(self.uri_for("bounces"), params=params)
         return json_to_py(response)
 
-    def clicks(self, date="", page=1, page_size=1000, order_field="date", order_direction="asc"):
+    def clicks(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the clicks for this journey email."""
-        params = {
-            "date": date,
-            "page": page,
-            "pagesize": page_size,
-            "orderfield": order_field,
-            "orderdirection": order_direction}
+        params = {}
+        if date is not None:
+            params["date"] = date
+        if page is not None:
+            params["page"] = page
+        if page_size is not None:
+            params["pagesize"] = page_size
+        if order_direction is not None:
+            params["orderdirection"] = order_direction
         response = self._get(self.uri_for("clicks"), params=params)
         return json_to_py(response)
 
-    def opens(self, date="", page=1, page_size=1000, order_field="date", order_direction="asc"):
+    def opens(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the opens for this journey email."""
-        params = {
-            "date": date,
-            "page": page,
-            "pagesize": page_size,
-            "orderfield": order_field,
-            "orderdirection": order_direction}
+        params = {}
+        if date is not None:
+            params["date"] = date
+        if page is not None:
+            params["page"] = page
+        if page_size is not None:
+            params["pagesize"] = page_size
+        if order_direction is not None:
+            params["orderdirection"] = order_direction
         response = self._get(self.uri_for("opens"), params=params)
         return json_to_py(response)
 
-    def recipients(self, date="", page=1, page_size=1000, order_field="date", order_direction="asc"):
+    def recipients(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the recipients for this journey email."""
-        params = {
-            "date": date,
-            "page": page,
-            "pagesize": page_size,
-            "orderfield": order_field,
-            "orderdirection": order_direction}
+        params = {}
+        if date is not None:
+            params["date"] = date
+        if page is not None:
+            params["page"] = page
+        if page_size is not None:
+            params["pagesize"] = page_size
+        if order_direction is not None:
+            params["orderdirection"] = order_direction
         response = self._get(self.uri_for("recipients"), params=params)
         return json_to_py(response)
 
-    def unsubscribes(self, date="", page=1, page_size=1000, order_field="date", order_direction="asc"):
+    def unsubscribes(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the unsubscribes for this journey email."""
-        params = {
-            "date": date,
-            "page": page,
-            "pagesize": page_size,
-            "orderfield": order_field,
-            "orderdirection": order_direction}
+        params = {}
+        if date is not None:
+            params["date"] = date
+        if page is not None:
+            params["page"] = page
+        if page_size is not None:
+            params["pagesize"] = page_size
+        if order_direction is not None:
+            params["orderdirection"] = order_direction
         response = self._get(self.uri_for("unsubscribes"), params=params)
         return json_to_py(response)
 
--- test/test_journey_email.py
+++ test/test_journey_email.py
@@ -1,3 +1,4 @@
+from six.moves.urllib.parse import quote
 import unittest
 
 from createsend.journey_email import JourneyEmail
@@ -5,14 +6,14 @@ from createsend.journey_email import JourneyEmail
 
 class JourneyEmailTestCase(object):
 
-    def test_bounces(self):
-        self.journey_email.stub_request(self.uri_for("bounces"), "journey_email_bounces.json")
+    def test_bounces_no_params(self):
+        self.journey_email.stub_request(self.no_param_uri_for("bounces"), "journey_email_bounces_no_params.json")
         bounces = self.journey_email.bounces()
         self.assertEqual(len(bounces.Results), 2)
         bounce_one = bounces.Results[0]
         self.assertEqual(bounce_one.EmailAddress, "asdf@softbouncemyemail.comX")
         self.assertEqual(bounce_one.BounceType, "Soft")
-        self.assertEqual(bounce_one.Date, "2019-08-14 14:24:00")
+        self.assertEqual(bounce_one.Date, "2019-08-20 14:24:00")
         self.assertEqual(bounce_one.Reason, "Soft Bounce - Dns Failure")
         self.assertEqual(bounces.ResultsOrderedBy, "Date")
         self.assertEqual(bounces.OrderDirection, "ASC")
@@ -22,8 +23,25 @@ class JourneyEmailTestCase(object):
         self.assertEqual(bounces.TotalNumberOfRecords, 2)
         self.assertEqual(bounces.NumberOfPages, 1)
 
-    def test_clicks(self):
-        self.journey_email.stub_request(self.uri_for("clicks"), "journey_email_clicks.json")
+    def test_bounces_with_params(self):
+        self.journey_email.stub_request(self.param_uri_for("bounces", "2019-01-01", 1, 10, "desc"), "journey_email_bounces_with_params.json")
+        bounces = self.journey_email.bounces(date="2019-01-01", page=1, page_size=10, order_direction="desc")
+        self.assertEqual(len(bounces.Results), 2)
+        bounce_one = bounces.Results[0]
+        self.assertEqual(bounce_one.EmailAddress, "asdf@hardbouncemyemail.com")
+        self.assertEqual(bounce_one.BounceType, "Hard")
+        self.assertEqual(bounce_one.Date, "2019-08-21 04:26:00")
+        self.assertEqual(bounce_one.Reason, "Hard Bounce")
+        self.assertEqual(bounces.ResultsOrderedBy, "Date")
+        self.assertEqual(bounces.OrderDirection, "DESC")
+        self.assertEqual(bounces.PageNumber, 1)
+        self.assertEqual(bounces.PageSize, 10)
+        self.assertEqual(bounces.RecordsOnThisPage, 2)
+        self.assertEqual(bounces.TotalNumberOfRecords, 2)
+        self.assertEqual(bounces.NumberOfPages, 1)
+
+    def test_clicks_no_params(self):
+        self.journey_email.stub_request(self.no_param_uri_for("clicks"), "journey_email_clicks_no_params.json")
         clicks = self.journey_email.clicks()
         self.assertEqual(len(clicks.Results), 2)
         click_one = clicks.Results[0]
@@ -45,8 +63,31 @@ class JourneyEmailTestCase(object):
         self.assertEquals(clicks.TotalNumberOfRecords, 2)
         self.assertEquals(clicks.NumberOfPages, 1)
 
-    def test_opens(self):
-        self.journey_email.stub_request(self.uri_for("opens"), "journey_email_opens.json")
+    def test_clicks_with_params(self):
+        self.journey_email.stub_request(self.param_uri_for("clicks", "2019-01-01", 1, 10, "desc"), "journey_email_clicks_with_params.json")
+        clicks = self.journey_email.clicks(date="2019-01-01", page=1, page_size=10, order_direction="desc")
+        self.assertEqual(len(clicks.Results), 2)
+        click_one = clicks.Results[0]
+        self.assertEqual(click_one.EmailAddress, "asdf+2@example.com")
+        self.assertEqual(click_one.Date, "2019-08-19 10:24:00")
+        self.assertEqual(click_one.URL, "https://example.com")
+        self.assertEqual(click_one.IPAddress, "198.148.196.144")
+        self.assertEqual(click_one.Latitude, -33.8591)
+        self.assertEqual(click_one.Longitude, 151.200195)
+        self.assertEqual(click_one.City, "Sydney")
+        self.assertEqual(click_one.Region, "New South Wales")
+        self.assertEqual(click_one.CountryCode, "AU")
+        self.assertEqual(click_one.CountryName, "Australia")
+        self.assertEquals(clicks.ResultsOrderedBy, "Date")
+        self.assertEquals(clicks.OrderDirection, "DESC")
+        self.assertEquals(clicks.PageNumber, 1)
+        self.assertEquals(clicks.PageSize, 10)
+        self.assertEquals(clicks.RecordsOnThisPage, 2)
+        self.assertEquals(clicks.TotalNumberOfRecords, 2)
+        self.assertEquals(clicks.NumberOfPages, 1)
+
+    def test_opens_no_params(self):
+        self.journey_email.stub_request(self.no_param_uri_for("opens"), "journey_email_opens_no_params.json")
         opens = self.journey_email.opens()
         self.assertEqual(len(opens.Results), 2)
         open_one = opens.Results[0]
@@ -67,8 +108,30 @@ class JourneyEmailTestCase(object):
         self.assertEquals(opens.TotalNumberOfRecords, 2)
         self.assertEquals(opens.NumberOfPages, 1)
 
-    def test_recipients(self):
-        self.journey_email.stub_request(self.uri_for("recipients"), "journey_email_recipients.json")
+    def test_opens_with_params(self):
+        self.journey_email.stub_request(self.param_uri_for("opens", "2019-01-01", 1, 10, "desc"), "journey_email_opens_with_params.json")
+        opens = self.journey_email.opens(date="2019-01-01", page=1, page_size=10, order_direction="desc")
+        self.assertEqual(len(opens.Results), 2)
+        open_one = opens.Results[0]
+        self.assertEqual(open_one.EmailAddress, "asdf+2@example.com")
+        self.assertEqual(open_one.Date, "2019-08-19 10:24:00")
+        self.assertEqual(open_one.IPAddress, "198.148.196.144")
+        self.assertEqual(open_one.Latitude, -33.8591)
+        self.assertEqual(open_one.Longitude, 151.200195)
+        self.assertEqual(open_one.City, "Sydney")
+        self.assertEqual(open_one.Region, "New South Wales")
+        self.assertEqual(open_one.CountryCode, "AU")
+        self.assertEqual(open_one.CountryName, "Australia")
+        self.assertEquals(opens.ResultsOrderedBy, "Date")
+        self.assertEquals(opens.OrderDirection, "DESC")
+        self.assertEquals(opens.PageNumber, 1)
+        self.assertEquals(opens.PageSize, 10)
+        self.assertEquals(opens.RecordsOnThisPage, 2)
+        self.assertEquals(opens.TotalNumberOfRecords, 2)
+        self.assertEquals(opens.NumberOfPages, 1)
+
+    def test_recipients_no_params(self):
+        self.journey_email.stub_request(self.no_param_uri_for("recipients"), "journey_email_recipients_no_params.json")
         recipients = self.journey_email.recipients()
         self.assertEqual(len(recipients.Results), 4)
         recipient_one = recipients.Results[0]
@@ -82,8 +145,23 @@ class JourneyEmailTestCase(object):
         self.assertEqual(recipients.TotalNumberOfRecords, 4)
         self.assertEqual(recipients.NumberOfPages, 1)
 
-    def test_unsubscribes(self):
-        self.journey_email.stub_request(self.uri_for("unsubscribes"), "journey_email_unsubscribes.json")
+    def test_recipients_with_params(self):
+        self.journey_email.stub_request(self.param_uri_for("recipients", "2019-01-01", 1, 10, "desc"), "journey_email_recipients_with_params.json")
+        recipients = self.journey_email.recipients(date="2019-01-01", page=1, page_size=10, order_direction="desc")
+        self.assertEqual(len(recipients.Results), 4)
+        recipient_one = recipients.Results[0]
+        self.assertEqual(recipient_one.EmailAddress, "asdf@hardbouncemyemail.com")
+        self.assertEqual(recipient_one.SentDate, "2019-08-21 04:26:00")
+        self.assertEqual(recipients.ResultsOrderedBy, "SentDate")
+        self.assertEqual(recipients.OrderDirection, "DESC")
+        self.assertEqual(recipients.PageNumber, 1)
+        self.assertEqual(recipients.PageSize, 10)
+        self.assertEqual(recipients.RecordsOnThisPage, 4)
+        self.assertEqual(recipients.TotalNumberOfRecords, 4)
+        self.assertEqual(recipients.NumberOfPages, 1)
+
+    def test_unsubscribes_no_params(self):
+        self.journey_email.stub_request(self.no_param_uri_for("unsubscribes"), "journey_email_unsubscribes_no_params.json")
         unsubscribes = self.journey_email.unsubscribes()
         self.assertEqual(len(unsubscribes.Results), 1)
         unsubscribe_one = unsubscribes.Results[0]
@@ -98,9 +176,29 @@ class JourneyEmailTestCase(object):
         self.assertEqual(unsubscribes.TotalNumberOfRecords, 1)
         self.assertEqual(unsubscribes.NumberOfPages, 1)
 
-    def uri_for(self, action):
-        return "journeys/email/%s/%s.json?date=&page=1&pagesize=1000&orderfield=date&orderdirection=asc" %\
+    def test_unsubscribes_with_params(self):
+        self.journey_email.stub_request(self.param_uri_for("unsubscribes", "2019-01-01", 1, 10, "desc"), "journey_email_unsubscribes_with_params.json")
+        unsubscribes = self.journey_email.unsubscribes(date="2019-01-01", page=1, page_size=10, order_direction="desc")
+        self.assertEqual(len(unsubscribes.Results), 1)
+        unsubscribe_one = unsubscribes.Results[0]
+        self.assertEqual(unsubscribe_one.EmailAddress, "asdf@example.com")
+        self.assertEqual(unsubscribe_one.Date, "2019-08-19 10:24:00")
+        self.assertEqual(unsubscribe_one.IPAddress, "198.148.196.144")
+        self.assertEqual(unsubscribes.ResultsOrderedBy, "Date")
+        self.assertEqual(unsubscribes.OrderDirection, "DESC")
+        self.assertEqual(unsubscribes.PageNumber, 1)
+        self.assertEqual(unsubscribes.PageSize, 10)
+        self.assertEqual(unsubscribes.RecordsOnThisPage, 1)
+        self.assertEqual(unsubscribes.TotalNumberOfRecords, 1)
+        self.assertEqual(unsubscribes.NumberOfPages, 1)
+
+    def no_param_uri_for(self, action):
+        return "journeys/email/%s/%s.json" %\
                (self.journey_email_id, action)
+
+    def param_uri_for(self, action, date, page, pagesize, orderdirection):
+        return "journeys/email/%s/%s.json?date=%s&page=%s&pagesize=%s&orderdirection=%s" %\
+               (self.journey_email_id, action, quote(date, ''), page, pagesize, orderdirection)
 
 
 class OAuthCampaignTestCase(unittest.TestCase, JourneyEmailTestCase):
