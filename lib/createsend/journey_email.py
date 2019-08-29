48b996c548864fdd3ffd747f9533b7fd602e3645
Zachary Hartmann <hartmann.zachary@gmail.com>
2019-08-29 11:20:18 +1000

    Improved efficiency and removed unused import

--- lib/createsend/journey_email.py
+++ lib/createsend/journey_email.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-import json
-
 from createsend.createsend import CreateSendBase
 from createsend.utils import json_to_py
 
@@ -15,62 +13,26 @@ class JourneyEmail(CreateSendBase):
 
     def bounces(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the bounces for this journey email."""
-        params = {}
-        if date is not None:
-            params["date"] = date
-        if page is not None:
-            params["page"] = page
-        if page_size is not None:
-            params["pagesize"] = page_size
-        if order_direction is not None:
-            params["orderdirection"] = order_direction
-        response = self._get(self.uri_for("bounces"), params=params)
-        return json_to_py(response)
+        return self.get_journey_email_response(date, page, page_size, order_direction, "bounces")
 
     def clicks(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the clicks for this journey email."""
-        params = {}
-        if date is not None:
-            params["date"] = date
-        if page is not None:
-            params["page"] = page
-        if page_size is not None:
-            params["pagesize"] = page_size
-        if order_direction is not None:
-            params["orderdirection"] = order_direction
-        response = self._get(self.uri_for("clicks"), params=params)
-        return json_to_py(response)
+        return self.get_journey_email_response(date, page, page_size, order_direction, "clicks")
 
     def opens(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the opens for this journey email."""
-        params = {}
-        if date is not None:
-            params["date"] = date
-        if page is not None:
-            params["page"] = page
-        if page_size is not None:
-            params["pagesize"] = page_size
-        if order_direction is not None:
-            params["orderdirection"] = order_direction
-        response = self._get(self.uri_for("opens"), params=params)
-        return json_to_py(response)
+        return self.get_journey_email_response(date, page, page_size, order_direction, "opens")
 
     def recipients(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the recipients for this journey email."""
-        params = {}
-        if date is not None:
-            params["date"] = date
-        if page is not None:
-            params["page"] = page
-        if page_size is not None:
-            params["pagesize"] = page_size
-        if order_direction is not None:
-            params["orderdirection"] = order_direction
-        response = self._get(self.uri_for("recipients"), params=params)
-        return json_to_py(response)
+        return self.get_journey_email_response(date, page, page_size, order_direction, "recipients")
 
     def unsubscribes(self, date=None, page=None, page_size=None, order_direction=None):
         """Retrieves the unsubscribes for this journey email."""
+        return self.get_journey_email_response(date, page, page_size, order_direction, "unsubscribes")
+
+    def get_journey_email_response(self, date, page, page_size, order_direction, uri):
+        """Retrieves information for the journey email - based on theuri"""
         params = {}
         if date is not None:
             params["date"] = date
@@ -80,8 +42,9 @@ class JourneyEmail(CreateSendBase):
             params["pagesize"] = page_size
         if order_direction is not None:
             params["orderdirection"] = order_direction
-        response = self._get(self.uri_for("unsubscribes"), params=params)
+        response = self._get(self.uri_for(uri), params=params)
         return json_to_py(response)
 
     def uri_for(self, action):
         return "/journeys/email/%s/%s.json" % (self.journey_email_id, action)
+
