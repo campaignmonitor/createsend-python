16ae7e24980a89489af419298141785934eb4e7b
Henry Soesanto <henrys@campaignmonitor.com>
2021-11-25 12:42:45 +1100

    EL-281 - Add more examples to utilize the filter of tags and dates

--- samples/clients.py
+++ samples/clients.py
@@ -5,18 +5,34 @@ auth = {
   'refresh_token': 'YOUR_REFRESH_TOKEN' }
 clientId = 'YOUR_CLIENT_ID'
 
+
 cs = CreateSend(auth)
 client = Client(auth, clientId)
 
 # Get list of sent campaigns
 print("List of sent campaigns:")
-campaigns = []
 pageNumber = 1
 pagedCampaigns = client.campaigns(page = 1)
 numberOfPages = pagedCampaigns.NumberOfPages
 while pageNumber <= numberOfPages:
   if (pageNumber > 1):
     pagedCampaigns = client.campaigns(page = pageNumber)
+  
+  print("  Page: %d" % pageNumber)
+  for cm in pagedCampaigns.Results:
+    print("    - %s" % cm.Subject)
+  
+  pageNumber = pageNumber + 1
+
+  
+# Get list of sent campaigns filtered by tags and date
+print("List of sent campaigns at 2021 with ABTest tag:")
+pageNumber = 1
+pagedCampaigns = client.campaigns(page = 1, sent_from_date="2021-01-01", sent_to_date="2022-01-01", tags="ABTest")
+numberOfPages = pagedCampaigns.NumberOfPages
+while pageNumber <= numberOfPages:
+  if (pageNumber > 1):
+    pagedCampaigns = client.campaigns(page = pageNumber, sent_from_date="2021-01-01", sent_to_date="2022-01-01", tags="ABTest")
   
   print("  Page: %d" % pageNumber)
   for cm in pagedCampaigns.Results:
