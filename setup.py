1c0a29ed056ee943fc4aa6726453a338b342d29c
Mike Manger <mike.manger@lighthouseuk.net>
2024-12-05 12:25:01 +0000

    Remove six requirement

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ setup(
 ***REMOVED******REMOVED***url="http://campaignmonitor.github.io/createsend-python/",
 ***REMOVED******REMOVED***license="MIT",
 ***REMOVED******REMOVED***keywords="createsend campaign monitor email",
-***REMOVED******REMOVED***install_requires=[
-***REMOVED******REMOVED******REMOVED******REMOVED***'six>=1.10',
-***REMOVED******REMOVED***],
 ***REMOVED******REMOVED***packages=find_packages('lib'),
 ***REMOVED******REMOVED***package_dir={'': 'lib'},
 ***REMOVED******REMOVED***package_data={'': ['cacert.pem']},
