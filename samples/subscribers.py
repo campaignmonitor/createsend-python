f4e3b8b7121fb3c748deb39cb0ee8126fe7729fd
AlexisT-CM <alexis.tzanos@meetmarigold.com>
2025-01-23 12:08:15 +1100

    Update examples in subscribers.py file

--- samples/subscribers.py
+++ samples/subscribers.py
@@ -6,9 +6,29 @@ auth = {
 listId = 'YOUR_LIST_ID'
 emailAddress = 'YOUR_SUBSCRIBER_EMAIL_ADDRESS'
 
+subscriberName = "YOUR_SUBSCRIBER_NAME"
+subscriberCustomFields = []
+subscriberResubscribed = False
+subscriberConsentToTrack = 'Unchanged'
+subscriberMobileNumber = "+61491570006" # This is a reserved mobile number by the Australian Communications and Media Authority
+subscriberConsentToSendSms = "Yes"
+
 subscriber = Subscriber(auth, listId, emailAddress)
 
 # Get the details for a subscriber
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***print(property, ":", value)
+***REMOVED***
+# Adding a subscriber
+***REMOVED***#This implemntation defaults the value of 'restart_subscription_based_autoresponders' to false
+subscriber.add(listId, emailAddress, subscriberName, subscriberCustomFields, subscriberResubscribed, subscriberConsentToTrack)
+
+# Adding a subscriber with a mobile number
+***REMOVED***#This implemntation defaults the value of 'restart_subscription_based_autoresponders' to false
+***REMOVED***#This also sets the default value of 'consent_to_track_sms' to 'unchanged', meaning new users will not receive SMS communications by default."
+subscriber.add(listId, emailAddress, subscriberName, subscriberCustomFields, subscriberConsentToTrack, mobile_Number=subscriberMobileNumber)
+
+#Alternative to set SMS tracking permissions 
+***REMOVED***# This implemntation defaults the value of 'restart_subscription_based_autoresponders' to false
+subscriber.add(listId, emailAddress, subscriberName, subscriberCustomFields, subscriberConsentToTrack, mobile_Number=subscriberMobileNumber, consent_to_track_sms=subscriberConsentToSendSms)
---
+++ samples/test.py
@@ -0,0 +1,39 @@
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***print(subs.Name)
+
+testSubscriber =***REMOVED***Subscriber({'api_key': key}, None, "Alexis.tzanos@meetmarigold.com")
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+# testSubscriber =***REMOVED***Subscriber.({'api_key': key}, testList.list_id, "SMSTRTESTE3@example.com")
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***print(property, ":", value)
