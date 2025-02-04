8dfed92a3ae84d925cca0baedacf79aca7f9ca89
AlexisT-CM <alexis.tzanos@meetmarigold.com>
2025-02-04 13:54:27 +1100

    Update setup.py to 9.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="createsend",
-    version='9.1.2',
+    version='9.1.3',
     description="A library which implements the complete functionality of the Campaign Monitor API.",
     author='Campaign Monitor',
     author_email='support@campaignmonitor.com',
